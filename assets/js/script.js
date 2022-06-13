5c0d71488c6a948f53ca6046936e525f0ea6474e
Lorreina Guyett <slorreina369@gmail.com>
2022-06-13 12:10:00 -0700

    created function that will use longt and latt to fetch weather api info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var apiKey = "057a07a785cfb642d41eff0a4ed06de1";
-var apiBase = "https://openweathermap.org/data/2.5/find";
+var apiBase = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 var geoKey = "126390888670417e15888818x77433";
 var searchBtn = document.querySelector(".btn");
 var getCity = document.querySelector("#search-city");
@@ -11,14 +11,30 @@ var searchCity = function(event){
     var city = getCity.value.trim();
 
     if(city){
-        console.log(city);
+        getLoc(city);
         getCity.value = "";
     } else{
         alert("Please enter a location for weather information");
     }
+
 };
 
-var getWeather = function(city){
+// this is solely to get the weather api info
+var findWeather = function(lattitude, longitude){
+    var cityData = apiBase + lattitude + "&lon=" + longitude + "&appid=" + apiKey;
+
+    fetch(cityData)
+    .then(function(response){
+        if(response.ok){
+            response.json().then
+            (function(data){
+                console.log(cityData)
+            })
+        }
+    })
+};
+
+var getLoc = function(city){
     var geoLocate="https://geocode.xyz/" + city + "?json=1";
     
     fetch(geoLocate)
@@ -26,18 +42,20 @@ var getWeather = function(city){
     
         if(response.ok){
             response.json().then(function(data){
+                // retrieve the longitude and lattitude of this location
                 var dataLong = data.longt;
                 var dataLatt = data.latt;
 
-                console.log(dataLong + ", " + dataLatt);
-            })
+                console.log(dataLatt + ", " + dataLong);
+                findWeather(dataLatt, dataLong);
+                console.log(geoLocate);
+            });
+        }else{
+            alert("Error: Location Not Found");
         }
     })
-    console.log(geoLocate);
 };
 
+getLoc("Portland,Oregon")
 
-
-
-getWeather("Portland,US");
 searchBtn.addEventListener("click", searchCity);
