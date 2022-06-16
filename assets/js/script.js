c03cd7c1499a56bef21ffc7959196eadd5d301c6
Lorreina Guyett <slorreina369@gmail.com>
2022-06-15 18:38:43 -0700

    weather icon added, search history function added, display previous history added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var geoKey = "126390888670417e15888818x77433";
 var searchBtn = document.querySelector(".btn");
 var getCity = document.querySelector("#search-city");
 var weatherNow = document.querySelector(".weather-now");
+var weatherIcon = document.querySelector(".weather-icon");
+var cities = [];
 
 // submit buttont to obtain information placed in search button
 var searchCity = function(event){
@@ -11,6 +13,7 @@ var searchCity = function(event){
     
     var city = getCity.value.trim();
 
+    $(weatherNow).find(".current-city").text(city);
     if(city){
         getLoc(city)
         .then(function(data){
@@ -18,7 +21,6 @@ var searchCity = function(event){
             var dataLong = data.longt;
             var dataLatt = data.latt;
 
-            console.log(dataLatt + ", " + dataLong);
             return findWeather(dataLatt, dataLong);
             
         })
@@ -27,7 +29,7 @@ var searchCity = function(event){
     } else{
         alert("Please enter a location for weather information");
     }
-
+    saveCity(city);
 };
 
 // this is solely to get the weather api info
@@ -45,7 +47,6 @@ var findWeather = function(lattitude, longitude){
 // take the geolocation and convert it to long/latt for weather api
 var getLoc = function(city){
     var geoLocate="https://geocode.xyz/" + city + "?json=1";
-    console.log(geoLocate);
     
     return fetch(geoLocate)
     .then(function(response){
@@ -62,25 +63,70 @@ var getLoc = function(city){
 var displayWeather = function(city){
     var currentDay = city.current;
     var date = moment(currentDay.dt * 1000).format('l');
-    console.log(date);
+    var currentIcon = currentDay.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/wn/" + currentIcon + ".png"
+
+    console.log(iconUrl);
+    $('#wicon').attr('src', iconUrl);
     $(weatherNow).find(".current-day").text(date);
     $(weatherNow).find(".temp").text(currentDay.temp);
     $(weatherNow).find(".wind").text(currentDay.wind_speed);
     $(weatherNow).find(".humid").text(currentDay.humidity);
     $(weatherNow).find(".index").text(currentDay.uvi);
 
+
     var forecastDays = city.daily.slice(1,6);
-    console.log(currentDay);
+    
     for(var i=0; i <forecastDays.length; i++){
         var dayCard = $(".day-card:nth-of-type(" + (i+1) + ")")
         var dayWeather = forecastDays[i];
         var showDate = moment(dayWeather.dt * 1000).format('l');
-        console.log(showDate);
+        var dayIcon = dayWeather.weather[0].icon;
+        var dayUrl = "http://openweathermap.org/img/wn/" + dayIcon + ".png"
+
+        dayCard.find(".weather-icon").attr('src', dayUrl);
         dayCard.find(".forecast-day").text(showDate);
         dayCard.find(".temp").text(dayWeather.temp.day);
         dayCard.find(".wind").text(dayWeather.wind_speed);
         dayCard.find(".humid").text(dayWeather.humidity);
-    }
+    };
+
+    getUvi(currentDay.uvi)
 };
 
+var getUvi = function(currentUvi){
+
+    if(currentUvi <= 2){
+        console.log("favorable");
+        $(weatherNow).find(".index").addClass("favorable");
+    } else if(currentUvi <=5){
+        console.log("moderate");
+        $(weatherNow).find(".index").addClass("moderate");
+    } else if(currentUvi >= 6){
+        console.log("severe");
+        $(weatherNow).find(".index").addClass("severe");
+    };
+};
+
+var saveCity = function(city){
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+
+};
+
+var loadCity = function(){
+    cities = JSON.parse(localStorage.getItem("cities"));
+
+    if(!cities){
+        cities = [];
+    };
+    for(var i=0; i<cities.length; i++){
+        cityHistory = cities[i];
+        var cityLi = $("<li>").text(cityHistory);
+        $(".search-history > ul").append(cityLi);
+
+    }
+}
+
 searchBtn.addEventListener("click", searchCity);
+loadCity();
